c098d9be6206d007e719fc9aa54eae13e6e9c15d
SamBirk4 <svbirk@q.com>
2022-05-05 15:36:43 -0600

    Edit ONE

--- project1.py
+++ project1.py
@@ -31,7 +31,7 @@ def user_profile() -> profile.Profile:
     
     while True:
         try:
-            user_height = input("Height (in): ")
+            user_height = input("Height (in): ") # Used to calculate BMI
 
             if not None == re.search('\D', user_height):
                 raise ValueError
@@ -42,7 +42,7 @@ def user_profile() -> profile.Profile:
         
     while True:
         try:
-            user_weight = input("Weight (lbs): ")
+            user_weight = input("Weight (lbs): ") # Used to calculate gain/loss
 
             if not None == re.search('\D', user_weight):
                 raise ValueError
@@ -66,30 +66,30 @@ def add_new_entry(p1):
             option = input("~").strip()
 
             if option.upper() =="DELETE":
-                collection.delete_many({})
+                collection.delete_many({}) # Will delete information in database 
                 break
             elif option.upper() == "ADD":
                 new_weight = input("\nenter weight (lbs): ")
-                p1.add_entry(new_weight)
+                p1.add_entry(new_weight) # Will append new weight to list of weights in order to calculate BMI and gain/loss
             elif option.upper() == "DONE":
-                load_weight()
+                load_weight() # Will run load function to print CSV to console if 'done' is selected 
                 break
             else:
                 raise ValueError('Invalid Input')
         except ValueError:
             print("\n\tPlease enter 'add' to continue. Otherwise enter 'delete' or 'done'.")
 
-header_lst = ["\nWeight (lbs) ", "Gain or Loss (lbs) ", "BMI ", "Date "]
+header_lst = ["\nWeight (lbs) ", "Gain or Loss (lbs) ", "BMI ", "Date "] # Headers for the information output from program
 
-def weight_track(p1):
+def weight_track(p1): # Writes output to csv file
     with open("fitness_track.csv", "a", newline = '') as fitness_log:
         writer = csv.writer(fitness_log)
         writer.writerow(p1.log_output())
     
 def load_weight():
     with open("fitness_track.csv", "r") as fitness_log:
-        csvFile = csv.reader(fitness_log)
-        for line in csvFile:
+        csvFile = csv.reader(fitness_log) # Prints all entries in Fitness_trac.csv to the console
+        for line in csvFile: # Prints lines to the database 
             first = True
             print(line)
             if line ==[]:
@@ -100,8 +100,8 @@ def load_weight():
                     continue
                 print(weight)
                 dict_val = {
-                    "Weight" : int(weight.strip("'")),
-                    "Gain or Loss" : line[1],
+                    "Weight (lbs)" : int(weight.strip("'")),
+                    "Gain or Loss (lbs)" : line[1],
                     "BMI" : line[2],
                     "Date" : line[3]
                 }
